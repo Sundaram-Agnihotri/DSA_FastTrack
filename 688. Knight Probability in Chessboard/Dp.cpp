ee012c0be8ed05f175c9d661f02afaff9ceab10b
Sundaram Agnihotri <110082729+Sundaram-Agnihotri@users.noreply.github.com>
2024-12-13 21:02:38 +0530

    Dp.cpp with approach

--- 688. Knight Probability in Chessboard/Dp.cpp
+++ 688. Knight Probability in Chessboard/Dp.cpp
@@ -1,3 +1,9 @@
+Appraoch : 
+
+1. You are given a N*N chessboard and the starting position of the knight in the chessboard.
+2. The rows and columns are 0 indexed, so the top-left square is (0, 0), and the bottom-right square is (N-1, N-1).
+3. You have to find the probability of knight to remain in the chessboard after exactly k number of moves.
+
 class Solution {
 public:
     double knightProbability(int n, int k, int row, int column) {
